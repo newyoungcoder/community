5f1ced96c080f1d946478ecfbb310c58ce8cfb6e
bee <1286312925@qq.com>
2021-07-02 11:35:35 +0800

    add replying function and fix login bug

--- src/main/java/life/majiang/community/service/CommentService.java
+++ src/main/java/life/majiang/community/service/CommentService.java
@@ -11,6 +11,8 @@ import life.majiang.community.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 public class CommentService {
 
@@ -23,7 +25,7 @@ public class CommentService {
     @Autowired
     private QuestionExtMapper questionExtMapper;
 
-
+    @Transactional
     public void insert(Comment comment) {
         if (comment.getParentId() == null || comment.getParentId() == 0) {
             throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
